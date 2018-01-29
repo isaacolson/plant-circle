bd743fabeab1849470aac681c5118ec347a33a69
isaacolson <olsonites@gmail.com>
2018-01-29 01:01:08 -0600

    Update plant_circle.py

--- plant_circle.py
+++ plant_circle.py
@@ -53,9 +53,9 @@ class Circle():
         sep_angle = 360/num_plants
         
         for i in range(num_plants)
-            angle = i*((math.pi/180)*sep_angle)
-            x_adjust = math.cos(angle)*(diameter/2)
-            y_adjust = math.sin(angle)*(diameter/2)
+            #angle = i*((math.pi/180)*sep_angle)
+            #x_adjust = math.cos(angle)*(diameter/2)
+            #y_adjust = math.sin(angle)*(diameter/2)
             #self.add_plant(x_pos + x_adjust, y_pos + y_adjust)
         log('{} plants added.'.format(num_plants),
             'success')
